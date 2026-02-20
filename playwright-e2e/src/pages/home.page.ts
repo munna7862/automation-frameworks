0354e6772c89f02ff927a903c954984c53616ece
munna7862 <66195984+munna7862@users.noreply.github.com>
2026-02-20 11:51:47 +0530

    Refactor HomePage to use updated imports and logging

--- playwright-e2e/src/pages/home.page.ts
+++ playwright-e2e/src/pages/home.page.ts
@@ -1,15 +1,13 @@
 import { BasePage } from '@core/base/base.page';
-import { Locator } from 'playwright/types/test';
-import { logger } from '@core/logger/logger';
+import { Locator } from '@playwright/test';
 
 export class HomePage extends BasePage {
-
   private get lblHomePage(): Locator { return this.page.locator("//a[text()=' Home']"); }
 
   public async verifyLandingPage() {
-    logger.info("Verifying landing on Home Page");
+    await this.logMessage('INFO', "Verifying landing on Home Page");
     let actualText = await this.doGetText(this.lblHomePage, "Checking if Home Page label is visible");
-    logger.info("Landed on Home Page successfully and Text is: " + actualText);
+    await this.logMessage('INFO', "Landed on Home Page successfully and Text is: " + actualText);
   }
 
 }
